579d06ac65dd48d2e88f6008d8ad0a8074799ef9
NC Boy <ncboyboyboy@gmail.com>
2026-03-05 07:41:13 +0700

    create auth helper functions

---
+++ src/utils/auth.js
@@ -0,0 +1,27 @@
+import api from "../lib/api";
+
+export const login = async (email, password) => {
+  const res = await api.post("/auth/login", {
+    email,
+    password,
+  });
+
+  const token = res.data.token;
+
+  localStorage.setItem("token", token);
+
+  return res.data.user;
+};
+
+export const logout = () => {
+  localStorage.removeItem("token");
+  window.location.href = "/login";
+};
+
+export const isAuthenticated = () => {
+  return !!localStorage.getItem("token");
+};
+
+export const getToken = () => {
+  return localStorage.getItem("token");
+};
